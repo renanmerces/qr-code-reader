ac3bf370610e8fb34c1149043cf28f9643dbd230
Renan Mercês <renan.merces@gmail.com>
2019-09-05 16:17:57 -0300

    Inclusão de biblioteca para leitura de QR Code

--- App.js
+++ App.js
@@ -1,66 +1,56 @@
-'use strict';
-
 import React, { Component } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View, Alert } from 'react-native';
-import { RNCamera } from 'react-native-camera';
+ 
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Linking,
+} from 'react-native';
+ 
+import QRCodeScanner from 'react-native-qrcode-scanner';
 
 export default class App extends Component {
-    takePicture = async() => {
-        if (this.camera) {
-            const options = { quality: 0.5, base64: true };
-            const data = await this.camera.takePictureAsync(options);
-            Alert.alert('Teste', String(data.uri));
-        }
-    };
-
-  render(){
-    return (
-        <View style={styles.container}>
-            <RNCamera
-                ref={ref => {
-                    this.camera = ref;
-                }}
-                style={styles.preview}
-                type={RNCamera.Constants.Type.back}
-                flashMode={RNCamera.Constants.FlashMode.on}
-                androidCameraPermissionOptions={{
-                    title: 'Permission to use camera',
-                    message: 'We need your permission to use your camera',
-                    buttonPositive: 'Ok',
-                    buttonNegative: 'Cancel',
-                }}
-                onGoogleVisionBarcodesDetected={({ barcodes }) => {
-                    console.log(barcodes);
-                }}
-            />
-            <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-                <TouchableOpacity onPress={() => this.takePicture()} style={styles.capture}>
-                <Text style={{ fontSize: 14 }}> SNAP </Text>
+    onSuccess = (e) => {
+        Linking
+          .openURL(e.data)
+          .catch(err => console.error('An error occured', err));
+    }
+     
+    render() {
+        return (
+            <QRCodeScanner
+            onRead={this.onSuccess}
+            topContent={
+                <Text style={styles.centerText}>
+                    Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.
+                </Text>
+            }
+            bottomContent={
+                <TouchableOpacity style={styles.buttonTouchable}>
+                    <Text style={styles.buttonText}>OK. Got it!</Text>
                 </TouchableOpacity>
-            </View>
-        </View>
-    );
-  }
+            }
+            />
+        );
+    }
 };
 
 const styles = StyleSheet.create({
-    container: {
+    centerText: {
         flex: 1,
-        flexDirection: 'column',
-        backgroundColor: 'black',
+        fontSize: 18,
+        padding: 32,
+        color: '#777',
     },
-    preview: {
-        flex: 1,
-        justifyContent: 'flex-end',
-        alignItems: 'center',
+    textBold: {
+        fontWeight: '500',
+        color: '#000',
     },
-    capture: {
-        flex: 0,
-        backgroundColor: '#fff',
-        borderRadius: 5,
-        padding: 15,
-        paddingHorizontal: 20,
-        alignSelf: 'center',
-        margin: 20,
+    buttonText: {
+        fontSize: 21,
+        color: 'rgb(0,122,255)',
+    },
+    buttonTouchable: {
+        padding: 16,
     },
 });
